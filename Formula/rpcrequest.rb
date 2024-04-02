2f7dd2cb63189c726193bfabddd2317b4f5c5ef6
Dragan Milic <dragan@netice9.com>
2024-04-02 14:39:44 +0200

    upgrade rpcrequest to 0.0.2

--- Formula/rpcrequest.rb
+++ Formula/rpcrequest.rb
@@ -1,8 +1,8 @@
 class Rpcrequest < Formula
   desc "Command-line utility for executing JSON-RPC"
   homepage "https://github.com/draganm/rpcrequest"
-  url "https://github.com/draganm/rpcrequest/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "7a3dfedbbf42971f8b5a0e9f7818c285906454c11046ca02b1bfb59ed4d08698"
+  url "https://github.com/draganm/rpcrequest/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "0fddf1abc8f76d22032df08228235d9510ce7944966f9fe3dcfdd246002d04ff"
 
   depends_on "go" => :build
 
